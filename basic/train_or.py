54be7fc5703040babc8daad192cd778c83680cb4
Yoidea <yoidea.cc@gmail.com>
2017-08-19 17:50:59 +0900

    Add predict code

--- basic/train_or.py
+++ basic/train_or.py
@@ -3,10 +3,10 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout
 from keras.optimizers import SGD
 
+
 x_train = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-y_train = np.array([[0], [0], [0], [1]])
+y_train = np.array([[0], [1], [1], [1]])
 x_test = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-y_test = np.array([[0], [0], [0], [1]])
 
 model = Sequential()
 model.add(Dense(1, input_dim=2, activation='sigmoid'))
@@ -14,7 +14,13 @@ model.add(Dense(1, input_dim=2, activation='sigmoid'))
 sgd = SGD(lr=0.2, momentum=0.02)
 model.compile(loss='mean_squared_error', optimizer=sgd)
 
-model.fit(x_train, y_train, epochs=10000, batch_size=1)
+print('Start learning OR')
+model.fit(x_train, y_train, epochs=2000, batch_size=1)
 predict = model.predict(x_test, batch_size=1)
-
 print(predict)
+
+print('Save model as model.json')
+json_data = model.to_json()
+open('model.json', 'w').write(json_data)
+print('Save weights as weight.hdf5')
+model.save_weights('weights.hdf5')
--- basic/train_xor.py
+++ basic/train_xor.py
@@ -3,10 +3,10 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout
 from keras.optimizers import SGD
 
+
 x_train = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 y_train = np.array([[0], [1], [1], [0]])
 x_test = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-y_test = np.array([[0], [0], [0], [1]])
 
 model = Sequential()
 model.add(Dense(2, input_dim=2, activation='sigmoid'))
@@ -15,7 +15,13 @@ model.add(Dense(1, activation='sigmoid'))
 sgd = SGD(lr=0.2, momentum=0.02)
 model.compile(loss='mean_squared_error', optimizer=sgd)
 
+print('Start learning XOR')
 model.fit(x_train, y_train, epochs=2000, batch_size=1)
 predict = model.predict(x_test, batch_size=1)
-
 print(predict)
+
+print('Save model as model.json')
+json_data = model.to_json()
+open('model.json', 'w').write(json_data)
+print('Save weights as weight.hdf5')
+model.save_weights('weights.hdf5')
