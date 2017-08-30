d75beeccd20841caf65584570ef89db9afd4c707
yoidea <yoidea.cc@gmail.com>
2017-08-30 17:57:37 +0900

    Update?

--- GAN/dcgan.py
+++ GAN/dcgan.py
@@ -2,35 +2,36 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Activation, Dense, Dropout, Conv2D, Flatten, MaxPooling2D
 from keras.layers.advanced_activations import LeakyReLU
-from keras.optimizers import Adagrad
-from keras.optimizers import Adam
+from keras.optimizers import SGD
 # from keras.utils.np_utils import to_categorical
 from PIL import Image
 import os
 
 
-image_dim = 100
+input_dim = 10
 batch_size = 5
 epochs = 500
-adam = Adam(lr=0.0001)
 
 
-def build_gen_model():
+def generator():
 	# Generatorを構築
 	model = Sequential()
-	model.add(Dense(256, input_dim=input_dim))
+	model.add(Dense(256, input_dim=100))
 	model.add(LeakyReLU(0.2))
 	model.add(Dense(512))
 	model.add(LeakyReLU(0.2))
 	model.add(Dense(1024))
 	model.add(Activation('sigmoid'))
+	# 不要かもしれない
+	sgd = SGD(lr=0.1, momentum=0.3, decay=1e-5)
+	model.compile(loss="binary_crossentropy", optimizer=sgd)
 	return model
 
 
-def build_dis_model():
-	# Generatorを構築
+def discriminator():
+	# Discriminatorを構築
 	model = Sequential()
-	model.add(Dense(1024, input_dim=input_dim))
+	model.add(Dense(1024, input_dim=10))
 	model.add(LeakyReLU(0.2))
 	model.add(Dense(512))
 	model.add(LeakyReLU(0.2))
@@ -38,6 +39,19 @@ def build_dis_model():
 	model.add(LeakyReLU(0.2))
 	model.add(Dense(1))
 	model.add(Activation('sigmoid'))
+	sgd = SGD(lr=0.1, momentum=0.1, decay=1e-5)
+	model.compile(loss="binary_crossentropy", optimizer=sgd)
+	return model
+
+
+def build_GAN():
+	# Generative Adversarial Network
+	G = generator()
+	D = discriminator()
+	model = Sequential()
+	model.add(G)
+	D.model.trainable = False
+	model.add(D)
 	return model
 
 
@@ -54,8 +68,9 @@ def main():
 	print('Loading datas')
 	(x_train, y_train) = load_data()
 	print('Building a model')
-	model = build_model()
-	model.compile(loss='categorical_crossentropy', optimizer=adam)
+	model = build_GAN()
+	sgd = SGD(0.1, momentum=0.3)
+	model.compile(loss="binary_crossentropy", optimizer=sgd)
 	print('Start learning')
 	model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs)
 	print('Save model as model.json')
